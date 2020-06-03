7d09a1771521973a79a3e761b2b23b1218538900
Thiago31 <tcastilho31@yahoo.com.br>
2020-06-03 17:28:33 -0300

    Estrutura inicial do site.

---
+++ public_html/js/common.js
@@ -0,0 +1,53 @@
+var defButton = {font: "100px Arial", selBackground: "orange",
+    unselBackground: "orangeRed", border: "green", borderWidth: 5, padding: 20};
+
+function button(text, x, y, config) {
+    this.text = text;
+    this.x = x;
+    this.y = y;
+    if (config === undefined) {
+        config = defButton;
+    }
+    this.font = config.font || defButton.font;
+    this.selBackground = config.selBackground || defButton.selBackground;
+    this.unselBackground = config.unselBackground || defButton.unselBackground;
+    this.padding = config.padding || defButton.padding;
+    this.border = config.border || defButton.border;
+    this.borderWidth = config.borderWidth || defButton.borderWidth;
+    this.visible = false;
+
+    this.draw = function (ctx) {
+        if (this.visible === false) {
+            return;
+        }
+        ctx.save();
+
+        ctx.font = this.font;
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+
+
+        let textMetrics = ctx.measureText(this.text);
+        this.yt = this.y - textMetrics.actualBoundingBoxAscent - this.padding;
+        this.yb = this.y + textMetrics.actualBoundingBoxDescent + this.padding;
+        this.xl = this.x - textMetrics.width / 2 - this.padding;
+        this.xr = this.x + textMetrics.width / 2 + this.padding;
+
+        ctx.fillStyle = this.border;
+        ctx.fillRect(this.xl - this.borderWidth, this.yt - this.borderWidth,
+                this.xr - this.xl + 2 * this.borderWidth,
+                this.yb - this.yt + 2 * this.borderWidth);
+        ctx.fillStyle = this.unselBackground;
+        ctx.fillRect(this.xl, this.yt, this.xr - this.xl, this.yb - this.yt);
+
+        ctx.fillStyle = "white";
+        ctx.fillText(this.text, this.x, this.y);
+
+        ctx.restore();
+
+    };
+    
+    this.isClickArea = function(xa, ya){
+        return (xa > this.xl && xa < this.xr && ya > this.yt && ya < this.yb);
+    };
+}
